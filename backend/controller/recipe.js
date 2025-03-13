dd5ceb3ba2c145572473397903f04fd7ff0d71e6
harindinikeshala <harindinikeshala@gmail.com>
2025-03-14 00:04:36 +0530

    Backend set up

---
+++ backend/controller/recipe.js
@@ -0,0 +1,27 @@
+const getRecipes = (req, res) => {
+    res.json({message:"hello world"});
+};
+
+const getRecipe = (req, res) => {
+    res.json({message:"hello world"});
+};
+
+const addRecipes = (req, res) => {
+    res.json({message:"hello world"});
+};
+
+const editRecipes = (req, res) => {
+    res.json({message:"hello world"});
+};
+
+const deleteRecipes = (req, res) => {
+    res.json({message:"hello world"});
+};
+
+module.exports = {
+    getRecipes,
+    getRecipe,
+    addRecipes,
+    editRecipes,
+    deleteRecipes
+};
