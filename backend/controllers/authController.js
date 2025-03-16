108e895c52fc1a15fed0284cc7d5c1a17b557993
harindinikeshala <harindinikeshala@gmail.com>
2025-03-16 22:09:29 +0530

    Sending Email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -23,6 +24,15 @@ export const register = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000 });
+
+        //sending welcome email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to our website',
+            text: `Hello ${name}, welcome to our website. Your account has been created with email id: ${email}.`
+        }
+        await transporter.sendMail(mailOptions);
 
         return res.json({ success: true, message: "User logged in successfully."});
 
